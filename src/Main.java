637fe7da1f1ed56e41d9b9a788553d79e65d1bfc
militinker <militinker@gmail.com>
2023-03-29 19:17:21 -0400

    Basic Structure of Car Project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicStructure honda=new BasicStructure();
+        honda.color="white";
+        honda.model="accord";
+        honda.power="500cc";
+    }
+}
